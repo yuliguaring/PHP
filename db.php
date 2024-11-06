045ae1fa916d3f666ced0b618dcb4c82848898ea
yuliguaring <yuliguaring@gmail.com>
2024-11-05 21:02:09 -0500

    conexion base de datos

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+
+$conexion = new mysqli("localhost", "root", "", "informacion");
+
+var_dump($conexion);
+
--- index.php
+++ index.php
@@ -7,8 +7,68 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="">
+    <form method = "get" action="">
+        <select name="" id="">
+            <option value="1"> opcion1 </option>
+            <option value="2"> opcion2 </option>
+        </select>
+        <br>   <br>
+
         
+
+        <table border="1">
+            <thead>
+                <th>Documento</th>
+                <th>Nombres</th>
+                <th>Telefono</th>
+
+            </thead>
+
+            <tbody>
+                <tr>
+
+                <td>12345687</td>
+                <td>yulieth</td>
+                <td>321654987</td>
+        
+                </tr>
+                
+                <tr>
+                  <td>555555</td>  
+                  <td>paola</td>
+                  <td>3133555669</td>
+
+                </tr>
+
+                 <tr>
+                    <td>666666</td>
+                    <td>juana</td>
+                    <td>6666666</td>
+                 </tr>
+
+                 <?php
+
+                 $inicio = 1;
+                    while ($inicio < 10 ){
+                        $inicio++;
+
+                ?>            
+
+<tr>
+                    <td>99999</td>
+                    <td>jjjj</td>
+                    <td>5555</td>
+                 </tr>
+                 <?php
+                    }
+                 ?>   
+
+            </tbody>
+
+
+        </table>
+            <br>
+        <button type="submit"> Enviar</button>
     </form>
 </body>
 </html>
